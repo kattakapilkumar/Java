508f390a909f53557b99db72c2f39f8b844290f3
Kapil Kumar Katta <kapilkatta123@gmail.com>
2017-09-15 19:55:27 +0530

    Index of

---
+++ src/collections/list/arraylist/IndexOf.java
@@ -0,0 +1,27 @@
+package collections.list.arraylist;
+
+
+
+import java.util.*;
+public class IndexOf 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner kapil = new Scanner(System.in);
+		
+		int a[] =  {1,2,1,2,1};
+		ArrayList al= new ArrayList();
+		
+		for(int kapili:a)
+		{
+			al.add(kapili);
+		}
+		System.out.println(al.indexOf(1));
+		
+		
+		
+
+	}
+
+}
