b506ea9e0bc754f4bd747f778518d1062950178d
kattakapilkumar <kapilkatta123@gmail.com>
2017-09-17 21:24:00 +0530

    arraytoarrayList

---
+++ src/collection/list/arraylist/ArrayToArrayList.java
@@ -0,0 +1,17 @@
+package collection.list.arraylist;
+
+import java.util.*;
+public class ArrayToArrayList {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		int array[]=new int[5];
+		
+		ArrayList arrayList = new ArrayList(Arrays.asList(array));
+		//List<Element> list = Arrays.asList(array);
+
+	}
+
+}
