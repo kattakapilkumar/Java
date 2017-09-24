0bf39b2036111cf904690d1561909a9f6d1d5c77
kattakapilkumar <kapilkatta123@gmail.com>
2017-09-24 13:44:00 +0530

    static block

---
+++ src/static_control_flow/Test.java
@@ -0,0 +1,10 @@
+package static_control_flow;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/static_control_flow/static_block/Test.java
@@ -0,0 +1,10 @@
+package static_control_flow.static_block;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
