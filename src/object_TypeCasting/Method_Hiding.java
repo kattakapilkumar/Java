40bd6ee4ef13f071a370c3fc845529b2e1a3c2a3
kattakapilkumar <kapilkatta123@gmail.com>
2017-09-24 13:14:47 +0530

    Object-Type_Casting

---
+++ src/object_TypeCasting/Method_Hiding.java
@@ -0,0 +1,10 @@
+package object_TypeCasting;
+
+public class Method_Hiding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/object_TypeCasting/Variables_Inheritance.java
@@ -0,0 +1,10 @@
+package object_TypeCasting;
+
+public class Variables_Inheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
