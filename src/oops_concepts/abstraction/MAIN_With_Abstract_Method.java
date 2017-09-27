840aeb4eb689deeae1931ab38792439ab79fc7f6
Kapil Kumar Katta <kapilkatta123@gmail.com>
2017-09-27 14:19:14 +0530

    oops

--- src/oops_concepts/abstraction/MAIN_With_Abstract_Method.java
+++ src/oops_concepts/abstraction/MAIN_With_Abstract_Method.java
@@ -1,10 +1,28 @@
 package oops_concepts.abstraction;
 
-public class MAIN_With_Abstract_Method {
+public class MAIN_With_Abstract_Method 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
+		
+		
 
+	}
+	public class Employee 
+	{
+		private String empCode;
+
+		
+		public String getEmpCode() {
+	        return empCode;
+	    }
+		
+		 public void setEmpCode(String empCode) {
+		        this.empCode = empCode;
+		    }
+		    
 	}
 
 }
