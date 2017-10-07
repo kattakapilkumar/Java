b1b47f0dd94c479a3a250c4497890128e0a27969
kattakapilkumar <kapilkatta123@gmail.com>
2017-10-07 11:23:06 +0530

    constructors

---
+++ src/multi_threading/enhancement_multi_threading/Constructor_2_ThreadGroup.java
@@ -0,0 +1,10 @@
+package multi_threading.enhancement_multi_threading;
+
+public class Constructor_2_ThreadGroup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
