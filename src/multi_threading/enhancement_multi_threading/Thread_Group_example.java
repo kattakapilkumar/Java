fdb848c4e0f317998f09c4040613d1e7ecb120be
kattakapilkumar <kapilkatta123@gmail.com>
2017-10-07 10:28:41 +0530

    threadsgroup

---
+++ src/multi_threading/enhancement_multi_threading/Thread_Group_example.java
@@ -0,0 +1,10 @@
+package multi_threading.enhancement_multi_threading;
+
+public class Thread_Group_example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
